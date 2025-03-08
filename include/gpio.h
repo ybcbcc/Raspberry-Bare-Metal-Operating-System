0f313d421e63234b77d74f586e2aa3d94fabcf78
ybcbcc <yaobianbuaikujiji@gmail.com>
2025-03-08 15:57:11 +0800

    gpio.h

--- include/gpio.h
+++ include/gpio.h
@@ -10,6 +10,10 @@
 #define GPIO_BASE       (PERIPHERAL_BASE + 0x200000)
 
 // GPIO function select registers
+//这些寄存器控制 GPIO 引脚的功能（输入、输出、特殊功能）
+/*
+每个GPIO占三位，每个寄存器占32位，所以设置IO的时候，将对应的IO设置成对应的状态，一共有三十个IO，例如我要设置18IO，就在GPFSEL1中找第24位到27位，设置成需要的模式即可。
+功能模式编码一共九种不赘述*/
 #define GPFSEL0         ((volatile uint32_t*)(GPIO_BASE + 0x00))
 #define GPFSEL1         ((volatile uint32_t*)(GPIO_BASE + 0x04))
 #define GPFSEL2         ((volatile uint32_t*)(GPIO_BASE + 0x08))
@@ -30,13 +34,13 @@
 #define UART0_ICR       ((volatile uint32_t*)(UART0_BASE + 0x44))
 
 // UART flags
-#define UART_FR_TXFF    (1 << 5)    // Transmit FIFO full
-#define UART_FR_RXFE    (1 << 4)    // Receive FIFO empty
+#define UART_FR_TXFF    (1 << 5)  // 发送 FIFO 满
+#define UART_FR_RXFE    (1 << 4)  // 接收 FIFO 空
 
 // UART control bits
-#define UART_CR_UARTEN  (1 << 0)    // UART enable
-#define UART_CR_TXE     (1 << 8)    // Transmit enable
-#define UART_CR_RXE     (1 << 9)    // Receive enable
+#define UART_CR_UARTEN  (1 << 0)  // 启用 UART
+#define UART_CR_TXE     (1 << 8)  // 启用 TX
+#define UART_CR_RXE     (1 << 9)  // 启用 RX
 
 // UART line control bits
 #define UART_LCRH_FEN   (1 << 4)    // Enable FIFOs
